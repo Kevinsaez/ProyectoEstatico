30beea29282eb28e4e4dc59fb81001b15de2f589
Kevinsaez <kevinsaez10@gmail.com>
2024-09-19 00:28:20 -0300

    modificacion modo oscuro boton

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const boton = document.getElementById('oscuro');
 const secciones = document.querySelectorAll('.seccion-clara');
+const iconos = document.querySelectorAll('.iconos-proyectos');
 
 function cambiarModo() {
   secciones.forEach(seccion => {
     seccion.classList.toggle('seccion-clara');
     seccion.classList.toggle('seccion-oscura');
   });
-  boton.innerHTML=(boton.innerHTML==='Modo claro') ? 'Modo oscuro':'Modo claro';
+  boton.innerHTML = (boton.innerHTML === 'Modo claro') ? 'Modo oscuro' : 'Modo claro';
+  iconos.forEach(icono => {
+    if (boton.innerHTML === 'Modo oscuro') {
+      icono.style.color = 'black'
+    } else {
+      icono.style.color = 'white'
+    }
+  });
 }
 
 boton.addEventListener('click', cambiarModo);
